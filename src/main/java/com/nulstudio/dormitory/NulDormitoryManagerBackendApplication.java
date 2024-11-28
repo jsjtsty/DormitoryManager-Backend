ded75d147d9b1816ec503a610a127e12f2fc4131
nullsty <jsjtsty@126.com>
2024-11-28 19:38:28 +0800

    feat: initial commit

---
+++ src/main/java/com/nulstudio/dormitory/NulDormitoryManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nulstudio.dormitory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NulDormitoryManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NulDormitoryManagerBackendApplication.class, args);
+	}
+
+}
